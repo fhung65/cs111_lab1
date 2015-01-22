4469cb93ad12aaa4071a98a111bb52680cc67478
fhung65 <fhung65@gmail.com>
2015-01-21 18:32:09 -0800

    quickly fixed a little bug in the temporary version of pipe that was making the output ugly, namely, executing only one of the subcommands

--- lab1-Skeleton/execute-command.c
+++ lab1-Skeleton/execute-command.c
@@ -60,7 +60,7 @@ execute_command (command_t c, int profiling)
 		case PIPE_COMMAND:
 			execute_command( c->u.command[0], profiling ) ;
 			// do stuff for redirect, maybe buffer
-			execute_command( c->u.command[0], profiling ) ;
+			execute_command( c->u.command[1], profiling ) ;
 			c->status = c->u.command[1]->status ;
 			break ;
 		case SEQUENCE_COMMAND:
